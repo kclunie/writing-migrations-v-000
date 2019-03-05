81ef02c9d08a37c80e3d091ec0d5897dcea20f11
Kristen Clunie <kcluniesa@yahoo.com>
2019-03-05 01:04:10 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -4,9 +4,6 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
     add_column :students, :grade, :integer 
     add_column :students, :birthdate, :string 
   end 
-    
-  
-  
   
   
 end 
